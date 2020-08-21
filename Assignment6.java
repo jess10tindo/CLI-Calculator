52c55c732f712448c1e724ea3e118cbef61e0751
jess10tindo <jatentindo@gmail.com>
2020-08-20 23:56:22 -0400

    Added looping with exit capability

--- Assignment6.java
+++ Assignment6.java
@@ -1,70 +1,81 @@
-package leturesAndAssignments;
+// This code will create a command line calculator
+
+package clicalc;
 
 import java.util.*;
 
 public class Assignment6 {
 
 	public static void main(String[] args) {
-	
-		Scanner input = new Scanner(System.in);
-		int answer;
 		
-		System.out.println("Welcome to the CLI Calculator");
-		System.out.println("Operations:");
-		System.out.println("a: Addition");
-		System.out.println("s: Subtraction");
-		System.out.println("m: Multiplication");
-		System.out.println("d: Division");
-		System.out.println("e: Exit");
-		System.out.println("");
-		System.out.println("Choose your operation: ");
-		
-		char chosenOp = input.next().charAt(0);
-		chosenOp = Character.toLowerCase(chosenOp);
-		
-		if (chosenOp == 'a'){
-			System.out.println("First Number: ");
-			int num1 = input.nextInt();
-			System.out.println("Second Number: ");
-			int num2 = input.nextInt();
+		char chosenOp = 'a';
+		while (chosenOp != 'e'){
 			
-			answer = num1 + num2;
-			System.out.println("Result: " + answer);
-		}
-		else if (chosenOp == 's'){
-			System.out.println("First Number: ");
-			int num1 = input.nextInt();
-			System.out.println("Second Number: ");
-			int num2 = input.nextInt();
+			Scanner input = new Scanner(System.in);
+			double answer = 0;
 			
-			answer = num1 - num2;
-			System.out.println("Result: " + answer);
-		}
-		else if (chosenOp == 'm'){
-			System.out.println("First Number: ");
-			int num1 = input.nextInt();
-			System.out.println("Second Number: ");
-			int num2 = input.nextInt();
+			System.out.println("Welcome to the CLI Calculator");
+			System.out.println("Operations:");
+			System.out.println("a: Addition");
+			System.out.println("s: Subtraction");
+			System.out.println("m: Multiplication");
+			System.out.println("d: Division");
+			System.out.println("e: Exit");
+			System.out.println();
+			System.out.println("Choose your operation: ");
 			
-			answer = num1 * num2;
-			System.out.println("Result: " + answer);
-		}
-		else if (chosenOp == 'd'){
-			System.out.println("First Number: ");
-			int num1 = input.nextInt();
-			System.out.println("Second Number: ");
-			int num2 = input.nextInt();
+			chosenOp = input.next().charAt(0);
+			chosenOp = Character.toLowerCase(chosenOp);
 			
-			if (num2 == 0){
-				System.out.println("Result: Cannot divide by 0.");
-			}
-			else{
-				answer = num1 / num2;
+			if (chosenOp == 'a'){
+				System.out.println("First Number: ");
+				int num1 = input.nextInt();
+				System.out.println("Second Number: ");
+				int num2 = input.nextInt();
+				answer = num1 + num2;
 				System.out.println("Result: " + answer);
 			}
-		}	
-		else{
-			System.out.println("You did not enter a valid operation. Please start again");
+			else if (chosenOp == 's'){
+				System.out.println("First Number: ");
+				int num1 = input.nextInt();
+				System.out.println("Second Number: ");
+				int num2 = input.nextInt();
+				answer = num1 - num2;
+				System.out.println("Result: " + answer);
+			}
+			else if (chosenOp == 'm'){
+				System.out.println("First Number: ");
+				int num1 = input.nextInt();
+				System.out.println("Second Number: ");
+				int num2 = input.nextInt();
+				answer = num1 * num2;
+				System.out.println("Result: " + answer);
+			}
+			else if (chosenOp == 'd'){	
+				System.out.println("First Number: ");
+				int num1 = input.nextInt();
+				System.out.println("Second Number: ");
+				int num2 = input.nextInt();
+				if (num2 == 0){
+					System.out.println("Result: Cannot divide by 0.");
+				}
+				else{
+					double num2div = num2; // convert num2 to double in order to get decimal values for double
+					answer = num1 / num2div;
+					// display answer to division problem with 2 decimals		
+				    System.out.println("Result: "); 
+				    System.out.printf("%.2f", answer); 
+				    System.out.println();
+				}
+			}	
+			else if (chosenOp == 'e'){
+				System.out.println("Goodbye!");
+			}
+			else{
+				System.out.println("You did not enter a valid operation. Please try again");
+			}
+			System.out.println("--------------------------------");
+			System.out.println();
 		}
-		}
-		}
+	}
+}
